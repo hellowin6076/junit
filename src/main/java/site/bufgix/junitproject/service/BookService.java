99e177faa749e690b0cc1700a98c7d8596735b79
lee.huimok <hellowin6076@gmail.com>
2022-08-21 02:04:57 +0900

    Repository 테스트 환경 세팅

--- src/main/java/site/bufgix/junitproject/service/BookService.java
+++ src/main/java/site/bufgix/junitproject/service/BookService.java
@@ -1,5 +1,8 @@
 package site.bufgix.junitproject.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
 }
--- src/test/java/site/bufgix/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/bufgix/junitproject/domain/BookRepositoryTest.java
@@ -1,5 +1,26 @@
 package site.bufgix.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class BookRepositoryTest {
+
+    @Autowired // DI
+    private BookRepository bookRepository;
+
+    // 책 등록
+    @Test
+    public void insert_test() {
+        System.out.println("insert_test excute");
+    }
+    // 책 목록보기
+
+    // 책 한건보기
+
+    // 책 수정
+
+    // 책 삭제
 
 }
