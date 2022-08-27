b1a54c00e85b917b691bd8bc32ce41b5b31df512
lee.huimok <hellowin6076@gmail.com>
2022-08-27 20:07:24 +0900

    book service insert book

--- src/main/java/site/bufgix/junitproject/service/BookService.java
+++ src/main/java/site/bufgix/junitproject/service/BookService.java
@@ -1,8 +1,33 @@
 package site.bufgix.junitproject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+import site.bufgix.junitproject.domain.Book;
+import site.bufgix.junitproject.domain.BookRepository;
+import site.bufgix.junitproject.web.dto.BookRespDto;
+import site.bufgix.junitproject.web.dto.BookSaveReqDto;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 책 등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto insert_book(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
+
+    // 책 목록보기
+
+    // 책 한건보기
+
+    // 책 삭제
+
+    // 책 수정
 
 }
